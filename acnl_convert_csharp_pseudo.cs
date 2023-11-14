6ac2eb38a12fbd0d3600dff2346cc1685ebebfb0
Lotte <joshdoes3d@gmail.com>
2023-11-15 00:51:30 +1100

    Update acnl_convert_csharp_pseudo.cs

--- acnl_convert_csharp_pseudo.cs
+++ acnl_convert_csharp_pseudo.cs
@@ -1,10 +1,25 @@
 public class ACNLConvertExample
 {
-    public void ConvertACPattern()
+    public byte[] ConvertACPattern()
     {
         byte[] ACNHpattern = File.ReadAllBytes(ACNH_Pattern_Example.nhd); // Read your pattern
         
-        byte[] ACNLpattern = new byte[0x26C]; // Create new bytes for ACNL
+        byte[] ACNLpattern = null;
+        int pixelDataLength = 0;
+        switch(ACNHpattern.Length) // Check Pattern Type
+        {
+            case 0x2A8:
+              ACNLpattern = new byte[0x26C]; // Create new bytes for ACNL Normal
+              pixelDataLength = 512;
+              break;
+            case 0x8A8:
+              ACNLpattern = new byte[0x870]; // Create new bytes for ACNL Pro
+              pixelDataLength = 2048;
+              break;  
+            default:
+              // Malformed file
+              return;
+        }
         
         switch(ACNHpattern[0x77]) // Check Pattern Type
         {
@@ -48,8 +63,8 @@ public class ACNLConvertExample
         Array.Copy(ACNHpattern, 0x58, ACNLpattern, 0x2C, 18); // Player Name (Trimmed)
         Array.Copy(ACNHpattern, 0x3C, ACNLpattern, 0x42, 18); // Island Name (Trimmed)
         
-        Array.Copy(patternDat, 0x54, ACNLbin, 0x2A, 2); // Player ID Hash
-        Array.Copy(patternDat, 0x38, ACNLbin, 0x40, 2); // Town ID Hash
+        Array.Copy(ACNHpattern, 0x54, ACNLpattern, 0x2A, 2); // Player ID Hash (or reset to 0xFF, 0xFF)
+        Array.Copy(ACNHpattern, 0x38, ACNLpattern, 0x40, 2); // Town ID Hash
         
         byte[] ACNHpalette = new byte[45]; // Create bytes to store ACNH palette
         Array.Copy(ACNHpattern, 0x78, ACNHpalette, 0, 45); // Store them
@@ -61,7 +76,12 @@ public class ACNLConvertExample
         }
         Array.Copy(ACNLpalette, 0x00, ACNLpattern, 0x58, 15);  // Store them
         
-        Array.Copy(ACNHpattern, 0xA5, ACNLpattern, 0x6C, 512); // Pixel Data
+        Array.Copy(ACNHpattern, 0xA5, ACNLpattern, 0x6C, pixelDataLength); // Pixel Data
+
+        if(ACNLpattern.Length == 0x870)
+            ACNLpattern = DataSortACNL(ACNLpattern);
+
+        return ACNLpattern;
     }
     
     public byte GetNearestColor(byte R, byte G, byte B)
@@ -93,5 +113,85 @@ public class ACNLConvertExample
         // ...and on and on...
         Color.Empty, // 0xFF, refer color table in info above
     };
+
+    public static byte[] DataSortACNL(byte[] data)
+    {
+        var dataSorted = new byte[0x870];
+        Array.Copy(data, 0x00, dataSorted, 0x00, 0x870);
+
+        Dictionary<(int, int), int> OffsetsProTexChunk = new Dictionary<(int, int), int>()
+        {
+          // Front Chunk
+          { ( 0x000, 0x200), 0x200 }, 
+          // Back Chunk
+          { ( 0x200, 0x400), 0x000 },
+          // Front Bottom Chunk
+          { ( 0x600, 0x700), 0x600 },
+          // Back Bottom Chunk
+          { ( 0x700, 0x800), 0x400 },
+          // Left Sleeve Chunk
+          { ( 0x400, 0x500), 0x500 },
+          // Right Sleeve Chunk
+          { ( 0x500, 0x600), 0x700 },
+        };
+
+        Dictionary<(int, int), int> OffsetsProStandeeTexChunk = new Dictionary<(int, int), int>()
+        {
+        // Top Left Chunk
+        { ( 0x000, 0x200), 0x000 }, 
+        // Bottom Left Chunk
+        { ( 0x200, 0x400), 0x400 },
+        // Top Right Chunk
+        { ( 0x400, 0x600), 0x200 },
+        // Bottom Right Chunk
+        { ( 0x600, 0x800), 0x600 }
+        };
+
+        // Check data type
+        if (data[0x69] != 0x06 && data[0x69] != 0x07)
+        {
+            for (var y = 0; y < 64; y++)
+            {
+                for (var x = 0; x < 64 / 2; x++)
+                {
+                    if (data[0x69] != 0x06 && data[0x69] != 0x07) // Ain't a hat? Off we hack!
+                    {
+                        var offset = (x >= 64 / 4 ? 0x200 : 0x0) + (y >= 64 / 2 ? 0x400 : 0x0);
+                        int index = offset + x % (64 / 4) + (y % (64 / 2)) * (64 / 4);
+                        if (data[0x69] == 0x08)
+                        {
+                            var newIndex = index;
+                            foreach (var kvp in OffsetsProStandeeTexChunk)
+                            {
+                                if (index >= kvp.Value && index < kvp.Value + (kvp.Key.Item2 - kvp.Key.Item1))
+                                {
+                                    newIndex = (index - kvp.Value) + kvp.Key.Item1;
+                                }
+                            }
+                            dataSorted[0x6C + newIndex] = data[0x6C + index];
+                        }
+                        else
+                        {
+                            var newIndex = index;
+                            foreach (var kvp in OffsetsProTexChunk)
+                            {
+                                if (index >= kvp.Value && index < kvp.Value + (kvp.Key.Item2 - kvp.Key.Item1))
+                                {
+                                    newIndex = (index - kvp.Value) + kvp.Key.Item1;
+                                }
+                            }
+                            dataSorted[0x6C + newIndex] = data[0x6C + index];
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            Array.Copy(data, 0x6C, dataSorted, 0x6C, 0x200);
+        }
+
+        return dataSorted;
+    }
 
 }
